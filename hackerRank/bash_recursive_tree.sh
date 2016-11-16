2c7e5910337e1820df725d9f76de8cd24a9223c1
sabertazimi <sabertazimi@gmail.com>
2016-11-16 19:53:38 +0800

    update(hroj-bash): polish code up
    issue sabertazimi/awesome-notes#88

--- hackerRank/bash_recursive_tree.sh
+++ hackerRank/bash_recursive_tree.sh
@@ -6,6 +6,9 @@
 # Distributed under terms of the MIT license.
 #
 
+# height of tree: 2^5 -> 2^4 -> ... -> 2^1
+# width  of tree: 2^5 -> 2^4 -> ... -> 2^1 (offset to center is 2^(i-1))
+
 declare -A matrix
 
 for((i=1;i<=63;i++));
@@ -17,29 +20,28 @@ do
 done
 
 function update_matrix {
-    local p1 p2 p3 p4 p5 p6
+    local itera power height p4 branch p6
     local q1 q2 q3
     local p13 p14
 
-    p1=$1
-    p2=$(echo $2-1|bc)
-    p1=$(echo $p1-1|bc)
-    p3=$(echo 2^$p2|bc)
-    p4=$(echo 2*$p3|bc)
-    p5=$(echo $p3/2|bc)
+    power=$(echo $2-1|bc)
+    itera=$(echo $1-1|bc)
+    height=$(echo 2^$power|bc)
+    p4=$(echo 2*$height|bc)
+    branch=$(echo $height/2|bc)
     p6=$3
 
-    for((q1=$p3;q1<$p4;q1++));
+    for((q1=$height;q1<$p4;q1++));
     do
-        if [ "$(echo $q1-$p3|bc)" -lt "$p5" ]
+        if [ "$(echo $q1-$height|bc)" -lt "$branch" ]
         then
-            # q2 = 18+p6-(p5-(q1-p3))
+            # q2 = 18+p6-(branch-(q1-height))
             # left offset to center
-            q2=$(echo 18+$p6-$(echo $p3/2-$(echo $q1-$p3|bc)|bc)|bc)
+            q2=$(echo 18+$p6-$(echo $branch-$(echo $q1-$height|bc)|bc)|bc)
 
-            # q3 = 18+p6+(p5-(q1-p3))
+            # q3 = 18+p6+(branch-(q1-height))
             # right offset to center
-            q3=$(echo 18+$p6+$(echo $p3/2-$(echo $q1-$p3|bc)|bc)|bc)
+            q3=$(echo 18+$p6+$(echo $branch-$(echo $q1-$height|bc)|bc)|bc)
 
             matrix[$q1,$q2]=1
             matrix[$q1,$q3]=1
@@ -49,13 +51,13 @@ function update_matrix {
         fi
     done
 
-    if [ $p1 -ge 1 ]
+    if [ $itera -ge 1 ]
     then
-        p13=$(echo $p6-$p3/2|bc)
-        p14=$(echo $p6+$p3/2|bc)
+        p13=$(echo $p6-$branch|bc)
+        p14=$(echo $p6+$branch|bc)
 
-        update_matrix $p1 $p2 $p13
-        update_matrix $p1 $p2 $p14
+        update_matrix $itera $power $p13
+        update_matrix $itera $power $p14
     fi
 }
 
