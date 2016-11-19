ce5bfc1c918d26f340800e01803cc4f32ad98b51
sabertazimi <sabertazimi@gmail.com>
2016-11-19 21:19:17 +0800

    add(ccfoj-20140305): try to solve cpu scheduling puzzle
    It's likely to solve this puzzle with dynamic programming.
    
    issue sabertazimi/awesome-notes#88

--- ccf/201403/cpu.cc
+++ ccf/201403/cpu.cc
@@ -13,6 +13,34 @@
 
 using namespace std;
 
+// dynamic programming
+// f(0,x,y,z)=max{x,y,z},
+// f(i,x,y,z)=min{f(i-1,x+a[i],y,z), f(i-1,x,y+a[i],z), f(i-1,x+c[i],y,z+c[i]),  f(i-1,x,y+c[i],z+c[i]), f(i-1,x+d[i],y+d[i],z+d[i])}.
+
+class Task {
+public:
+    int pid;
+    int sc, mc, scg, mcg;
+    Task(void) {
+        pid = sc = mc = scg = mcg = 0;
+    }
+    Task(int pid, int sc, int mc, int scg, int mcg) {
+        this->pid = pid;
+        this->sc = sc;
+        this->mc = mc;
+        this->scg = scg;
+        this->mcg = mcg;
+    }
+};
+
 int main(void) {
-    return 0;
+    int n;
+    cin >> n;
+
+    int sc, mc, scg, mcg;
+    vector<Task *> tasks(n);
+    for (int i = 0; i < n; i++) {
+        cin >> sc >> mc >> scg >> mcg;
+        tasks[i] = new Task(i, sc, mc, scg, mcg);
+    }
 }
