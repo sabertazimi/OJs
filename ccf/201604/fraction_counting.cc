3d2ac02b54c419df0977e5f70bdac793d8ab251d
sabertazimi <sabertazimi@gmail.com>
2016-12-06 23:13:41 +0800

    update(oj-ccf-201604): copy code
    issue sabertazimi/awesome-notes#88

--- ccf/201604/fraction_counting.cc
+++ ccf/201604/fraction_counting.cc
@@ -12,8 +12,25 @@
 
 using namespace std;
 
-int main(void) {
-    return 0;
+int vec[1000];
+
+int main(void){
+	int N;
+	cin>>N;
+
+	for(int i=0;i<N;i++){
+		cin>>vec[i];
+	}
+
+	int res=0;
+
+	for(int i=1;i<N-1;i++){
+		if(vec[i-1]<vec[i]&&vec[i+1]<vec[i]||vec[i-1]>vec[i]&&vec[i+1]>vec[i]){
+			res++;
+		}
+	}
+
+	cout<<res<<endl;
+	
+	return 0;
 }
-
-
--- ccf/201604/tetris.cc
+++ ccf/201604/tetris.cc
@@ -12,7 +12,66 @@
 
 using namespace std;
 
-int main(void) {
-    return 0;
-}
+bool board[16][15];
+bool block[5][5];
+int top[]={16,16,16,16,16};
+int bottom[]={0,0,0,0,0};
 
+int main(void){
+	int pos;
+	for(int i=1;i<=15;i++){
+		for(int j=1;j<=10;j++){
+			cin>>board[i][j];
+		}
+	}
+	for(int i=1;i<=4;i++){
+		for(int j=1;j<=4;j++){
+			cin>>block[i][j];
+		}
+	}
+	cin>>pos;
+	for(int j=1;j<=4;j++){
+		for(int i=1;i<=15;i++){
+			if(board[i][j+pos-1]){
+				top[j]=i;
+				break;
+			}
+		}
+	}
+	for(int j=1;j<=4;j++){
+		for(int i=4;i>=1;i--){
+			if(block[i][j]){
+				bottom[j]=i;
+				break;
+			}
+		}
+	}
+	
+	int min=99;
+	for(int i=1;i<=4;i++){
+		int tmp=15;
+		if(bottom[i]){
+			tmp=top[i]-bottom[i];
+		}
+		if(tmp<min){
+			min=tmp;
+		}
+	}
+	
+	for(int i=min;i<=15;i++){
+		for(int j=pos;j<=pos+3;j++){
+			if(block[i-min+1][j-pos+1]){
+				board[i][j]=1;
+			}
+		}
+	}
+		
+	for(int i=1;i<=15;i++){
+		for(int j=1;j<=10;j++){
+			cout<<board[i][j]<<' ';
+		}
+		cout<<endl;
+	}
+	
+	return 0;
+}
