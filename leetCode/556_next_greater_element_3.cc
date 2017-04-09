4a90a31412c72cae4cb9aa792c49fc8c6bd5700c
sabertazimi <sabertazimi@gmail.com>
2017-04-09 11:38:43 +0800

    add(oj-leetcode-556): Next Greater Element - Math/String
    issue sabertazimi/awesome-notes#88

--- leetCode/556_next_greater_element_3.cc
+++ leetCode/556_next_greater_element_3.cc
@@ -1,27 +1,15 @@
 class Solution {
 public:
     int nextGreaterElement(int n) {
-        int max = (numeric_limits<int>::max)();
-
-        if (n == max) {
-            return -1;
-        }
-
-        if (n & (1 << 30)) {
-            return ((n << 1) | 0x1);
-        } else {
-            return (n << 1);
-        }
-    }
-private:
-    int bitCount(unsigned int n) {
-        int res = 0;
-
-        while (n) {
-            res += n & 1;
-            n >>= 1;
-        }
-
-        return res;
+        // search start from tail for strn[i] < strn[i+1]
+        // search start from tail for strn[i] < strn[j]
+        // swap(strn[i], strn[j])
+        // reverse(strn.begin() + i + 1, strn.end());
+        // e.g 2319482 => 2319 48 2 (find) => 2319 84 2 (swap) => 2319 8 24 (reverse) => 2319824
+        // e.g 2319485432 => 2319 48 5432 (find) => 2319 58 4432 (swap) => 2319 5 23448 (reverse) => 2319523448
+        auto digits = to_string(n);
+        next_permutation(begin(digits), end(digits));
+        auto res = stoll(digits);
+        return (res > INT_MAX || res <= n) ? -1 : res;
     }
 };
