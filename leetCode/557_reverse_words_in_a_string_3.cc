5f73e64838dbb207810421db5caf4b0e62d2a3b7
sabertazimi <sabertazimi@gmail.com>
2017-04-09 11:03:49 +0800

    add(oj-leetcode-557): Reverse Words - String/Two Pointers
    issue sabertazimi/awesome-notes#88

--- leetCode/557_reverse_words_in_a_string_3.cc
+++ leetCode/557_reverse_words_in_a_string_3.cc
@@ -1,37 +1,15 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        vector<string> strs = split(s, ' ');
-        int n = strs.size();
-        string res = "";
-        string rev = "";
+        size_t front = 0;
 
-        if (n == 0) {
-            return res;
+        for (int i = 0; i <= s.length(); i++){
+            if (i == s.length() || s[i] == ' '){
+                reverse(&s[front], &s[i]);
+                front = i + 1;
+            }
         }
 
-        for (int i = 0; i < n - 1; i++) {
-            reverse(strs[i].begin(), strs[i].end());
-            res += (strs[i] + " ");
-        }
-
-        reverse(strs[n-1].begin(), strs[n-1].end());
-        res += strs[n-1];
-
-        return res;
-    }
-
-private:
-    vector<string> split(const std::string &s, char delim) {
-        vector<string> elems;
-        stringstream ss;
-        ss.str(s);
-        string item;
-
-        while (getline(ss, item, delim)) {
-            elems.push_back(item);
-        }
-
-        return elems;
+        return s;
     }
 };
