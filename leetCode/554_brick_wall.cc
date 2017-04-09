a6fad6126f24d7d766cd5adf08dd29a4824ac663
sabertazimi <sabertazimi@gmail.com>
2017-04-09 11:09:11 +0800

    add(oj-leetcode-554): Brick Wall - Hasp Map/Discretization
    issue sabertazimi/awesome-notes#88

--- leetCode/554_brick_wall.cc
+++ leetCode/554_brick_wall.cc
@@ -1,46 +1,14 @@
 class Solution {
 public:
-    int leastBricks(vector<vector<int>>& wall) {
-        vector< map<int, bool> > space;
+    int leastBricks(vector< vector<int> > &wall) {
+        unordered_map<int, int> edges;
+        auto res = wall.size();
 
-        int n = wall.size();
-
-        if (n == 0) {
-            return 0;
-        }
-
-        int m = wall[0].size();
-
-        if (m == 0) {
-            return 0;
-        }
-
-        int maxWidth = 0;
-        for (int i = 0; i < m; i++) {
-            maxWidth += wall[0][i];
-        }
-
-        for (int i = 0; i < n; i++) {
-            int width = 0;
-            space.push_back(map<int, bool>());
-            space[i][width] = false;
-
-            for (int j = 0; j < m - 1; j++) {
-                width += wall[i][j];
-                space[i][width] = true;
+        for (auto row : wall) {
+            for (auto i = 0, width = 0; i < row.size() - 1; i++) {
+                edges[width += row[i]]++;
+                res = min(res, wall.size() - edges[width]);
             }
-        }
-
-        int res = n;
-        for (int j = 1; j < maxWidth; j++) {
-            int cur = 0;
-            for (int i = 0; i < n; i++) {
-                if (space[i].find(j) == space[i].end()) {
-                    cur++;
-                }
-            }
-
-            res = min(res, cur);
         }
 
         return res;
