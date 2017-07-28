6e5867327328f8811079acd44a4283e7a7affc60
Yilong Liu <sabertazimi@gmail.com>
2017-07-28 14:57:10 +0800

    add(leetcode-190): Reverse Bits - Bits Manipulation

---
+++ leetCode/190_reverse_bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+	uint32_t reverseBits(uint32_t n) {
+		uint32_t m = 0;
+
+		for (int i = 0; i < 32; i++, n >>= 1) {
+			m <<= 1;
+			m |= n & 1;
+		}
+		
+		return m;
+	}
+};
