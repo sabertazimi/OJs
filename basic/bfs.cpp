49f9b64b76017f5c18487fc1da29f6121e506bf0
sabertazimi <sabertazimi@gmail.com>
2018-10-02 11:47:31 +0800

    update(basic-bfs): print shortest path

--- basic/bfs.cpp
+++ basic/bfs.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <utility>
 #include <queue>
+#include <stack>
 
 const int DIM = 5;
 
 class Algo {
   int maze[DIM][DIM];
+  std::pair<int, int> parent[DIM][DIM];
   bool visit[DIM][DIM];
   int dim;
 
@@ -16,6 +18,7 @@ public:
     for (int i = 0; i < dim; ++i) {
       for (int j = 0; j < dim; ++j) {
         this->maze[i][j] = maze[i][j];
+	this->parent[i][j] = std::make_pair(-1, -1);
         this->visit[i][j] = false;
       }
     }
@@ -31,6 +34,7 @@ public:
     std::queue<std::pair<int, int> > frontier;
 
     frontier.push(std::make_pair(0, 0));
+    this->parent[0][0] = std::make_pair(0, 0);
     this->visit[0][0] = true;
 
     while (!frontier.empty()) {
@@ -52,12 +56,35 @@ public:
 
         if (isInMaze && this->maze[row][col] == 0 && !this->visit[row][col]) {
           frontier.push(std::make_pair(row, col));
+    	  this->parent[row][col] = std::make_pair(cur.first, cur.second);
           this->visit[row][col] = true;
         }
       }
     }
 
     return false;
+  }
+
+  void printPath(void) {
+    std::stack<std::pair<int, int> > path;
+    std::pair<int, int> trav = this->parent[DIM - 1][DIM - 1];
+    path.push(std::make_pair(DIM - 1, DIM - 1));
+
+    while (trav.first != -1) {
+      path.push(trav);
+
+      if (trav.first == 0 && trav.second == 0) {
+        break;
+      }
+
+      trav = this->parent[trav.first][trav.second];
+    }
+
+    while(!path.empty()) {
+      trav = path.top();
+      path.pop();
+      std::cout << trav.first << ", " << trav.second << std::endl;
+    }
   }
 };
 
@@ -75,6 +102,10 @@ int main(void) {
   std::cout << "Maze resolved: "
             << (result ? "true" : "false")
             << std::endl;
+
+  if (result) {
+    algo.printPath();
+  }
 
   return 0;
 }
