3448e0644ad9aeaf510da435d04c34f0973565df
sabertazimi <sabertazimi@gmail.com>
2021-08-22 01:23:56 +0800

    dp(leetcode-300): longest increasing subsequence

---
+++ leetCode/300.test.ts
@@ -0,0 +1,15 @@
+import solution from './300';
+
+// Given an integer array nums,
+// return the length of the longest strictly increasing subsequence.
+// A subsequence is a sequence that can be derived from
+// an array by deleting some or no elements without changing the order of the remaining elements.
+// For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
+describe('LeetCode [300]', () => {
+  test('should AC', () => {
+    expect(solution([])).toBe(0);
+    expect(solution([10, 9, 2, 5, 3, 7, 101, 18])).toBe(4);
+    expect(solution([0, 1, 0, 3, 2, 3])).toBe(4);
+    expect(solution([7, 7, 7, 7, 7, 7, 7])).toBe(1);
+  });
+});
