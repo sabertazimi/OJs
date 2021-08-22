dc4e79336b10d31855a67b2d1351c605c91f843d
sabertazimi <sabertazimi@gmail.com>
2021-08-22 18:17:46 +0800

    array(leetcode-0006): string simulation zigzag conversion

---
+++ leetCode/0006.test.ts
@@ -0,0 +1,22 @@
+import solution from './0006';
+
+/**
+ * @author sabertazimi
+ * @license MIT
+ * @level medium
+ * @description zigzag-conversion
+ * The string "PAYPALISHIRING" is written in a zigzag pattern
+ * on a given number of rows like this:
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ * And then read line by line: "PAHNAPLSIIGYIR"
+ * Write the code that will take a string and make this conversion given a number of rows.
+ */
+describe('LeetCode [0006]', () => {
+  test('should AC', () => {
+    expect(solution('PAYPALISHIRING', 3)).toBe('PAHNAPLSIIGYIR');
+    expect(solution('PAYPALISHIRING', 4)).toBe('PINALSIGYAHRPI');
+    expect(solution('A', 1)).toBe('A');
+  });
+});
