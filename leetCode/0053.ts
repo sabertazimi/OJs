76916e15c1549bedd2b3a34aac12ab210b6511d2
sabertazimi <sabertazimi@gmail.com>
2021-08-23 23:03:40 +0800

    dp(leetcode-0053): maximum subarray (one variable store DP)

---
+++ leetCode/0053.ts
@@ -0,0 +1,13 @@
+export default function maxSubArray(nums: number[]): number {
+  let sum = 0;
+  let dp = nums[0];
+
+  for (let i = 0; i < nums.length; i++) {
+    // DP formula: f(i) = Math.max(f(i−1) + nums[i], nums[i])
+    // Solution : Max(f(i)), 0 <= i <= nums.length - 1;
+    sum = Math.max(sum + nums[i], nums[i]);
+    dp = Math.max(dp, sum);
+  }
+
+  return dp;
+}
