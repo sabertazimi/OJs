325b1a7b451d4577a75a49b90e912159dac4c0f0
sabertazimi <sabertazimi@gmail.com>
2021-08-23 22:19:46 +0800

    search(leetcode-0035): search insert position in sorted array

---
+++ leetCode/0035.ts
@@ -0,0 +1,17 @@
+export default function searchInsert(nums: number[], target: number): number {
+  let lo = 0;
+  let hi = nums.length - 1;
+
+  while (lo <= hi) {
+    const mid = (lo + hi) >> 1;
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      lo = mid + 1;
+    } else {
+      hi = mid - 1;
+    }
+  }
+
+  return lo;
+}
