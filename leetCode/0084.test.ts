59d06f8ccde2ccc39e9bb8c244dc1fbc5632c152
sabertazimi <sabertazimi@gmail.com>
2021-08-24 22:56:22 +0800

    stack(leetcode-0084): largest rectangle in histogram (increasing stack)

---
+++ leetCode/0084.test.ts
@@ -0,0 +1,17 @@
+import solution from './0084';
+
+/**
+ * @author sabertazimi
+ * @license MIT
+ * @level hard
+ * @description largest-rectangle-in-histogram
+ * Given an array of integers heights representing the histogram's bar height where the width of each bar is 1,
+ * return the area of the largest rectangle in the histogram.
+ */
+describe('LeetCode [0084]', () => {
+  test('should AC', () => {
+    expect(solution([0])).toStrictEqual(0);
+    expect(solution([2, 4])).toStrictEqual(4);
+    expect(solution([2, 1, 5, 6, 2, 3])).toStrictEqual(10);
+  });
+});
---
+++ leetCode/0084.ts
@@ -0,0 +1,27 @@
+export default function largestRectangleArea(heights: number[]): number {
+  heights.unshift(0);
+  heights.push(0);
+  let res = 0;
+
+  // Record indices of elements with increase height
+  const increaseStack = [];
+  increaseStack.push(0);
+
+  for (let i = 1, top = 0; i < heights.length; i++) {
+    while (
+      heights[i] < heights[(top = increaseStack[increaseStack.length - 1])]
+    ) {
+      increaseStack.pop();
+      res = Math.max(
+        res,
+        heights[top] * (i - increaseStack[increaseStack.length - 1] - 1)
+      );
+    }
+
+    // heights[i] >= heights[increaseStack.top()]
+    // Push index of higher element to stack
+    increaseStack.push(i);
+  }
+
+  return res;
+}
