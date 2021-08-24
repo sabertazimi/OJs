1d296e27857efae6dd42cf0b498e11d0029af437
sabertazimi <sabertazimi@gmail.com>
2021-08-24 22:13:02 +0800

    list(leetcode-0083): remove duplicates from sorted list

---
+++ leetCode/0083.test.ts
@@ -0,0 +1,22 @@
+import solution from './0083';
+import { arrayToList } from './List';
+
+/**
+ * @author sabertazimi
+ * @license MIT
+ * @level easy
+ * @description remove-duplicates-from-sorted-list
+ * Given the head of a sorted linked list,
+ * delete all duplicates such that each element appears only once.
+ * Return the linked list sorted as well.
+ */
+describe('LeetCode [0083]', () => {
+  test('should AC', () => {
+    expect(solution(arrayToList([]))).toStrictEqual(arrayToList([]));
+    expect(solution(arrayToList([1, 2]))).toStrictEqual(arrayToList([1, 2]));
+    expect(solution(arrayToList([1, 1, 2]))).toStrictEqual(arrayToList([1, 2]));
+    expect(solution(arrayToList([1, 1, 2, 3, 3]))).toStrictEqual(
+      arrayToList([1, 2, 3])
+    );
+  });
+});
