d05c76c2fd344bebcbb1ebeb0d7bc6621ced208b
sabertazimi <sabertazimi@gmail.com>
2021-08-24 23:25:49 +0800

    dp/stack(leetcode-0085): maximal rectangle

---
+++ leetCode/0085.test.ts
@@ -0,0 +1,27 @@
+import solution from './0085';
+
+/**
+ * @author sabertazimi
+ * @license MIT
+ * @level hard
+ * @description maximal-rectangle
+ * Given a rows x cols binary matrix filled with 0's and 1's,
+ * find the largest rectangle containing only 1's and return its area.
+ */
+describe('LeetCode [0085]', () => {
+  test('should AC', () => {
+    expect(solution([])).toStrictEqual(0);
+    expect(solution([['0']])).toStrictEqual(0);
+    expect(solution([['0', '0']])).toStrictEqual(0);
+    expect(solution([['1']])).toStrictEqual(1);
+    expect(solution([['1', '1']])).toStrictEqual(2);
+    expect(
+      solution([
+        ['1', '0', '1', '0', '0'],
+        ['1', '0', '1', '1', '1'],
+        ['1', '1', '1', '1', '1'],
+        ['1', '0', '0', '1', '0'],
+      ])
+    ).toStrictEqual(6);
+  });
+});
