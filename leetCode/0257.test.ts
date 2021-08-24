4913fa043016cb432e2ffc1028b00891636f09ed
sabertazimi <sabertazimi@gmail.com>
2021-08-24 21:47:46 +0800

    dfs/tree(leetcode-0257): binary tree paths

---
+++ leetCode/0257.test.ts
@@ -0,0 +1,26 @@
+import solution from './0257';
+import { arrayToTree } from './Tree';
+
+/**
+ * @author sabertazimi
+ * @license MIT
+ * @level easy
+ * @description binary-tree-paths
+ * Given the root of a binary tree,
+ * return all root-to-leaf paths in any order.
+ * A leaf is a node with no children.
+ */
+describe('LeetCode [0257]', () => {
+  test('should AC', () => {
+    expect(solution(arrayToTree([]))).toStrictEqual([]);
+    expect(solution(arrayToTree([1]))).toStrictEqual(['1']);
+    expect(solution(arrayToTree([1, 2, 3, 0, 5]))).toStrictEqual([
+      '1->2->5',
+      '1->3',
+    ]);
+    expect(solution(arrayToTree([1, 2, 3, 5, 0, 0, 5]))).toStrictEqual([
+      '1->2->5',
+      '1->3->5',
+    ]);
+  });
+});
---
+++ leetCode/0257.ts
@@ -0,0 +1,27 @@
+import { TreeNode } from './Tree';
+
+export default function binaryTreePaths(root: TreeNode | null): string[] {
+  const paths: string[] = [];
+
+  const dfs = (root: TreeNode, path: string) => {
+    if (root.left === null && root.right === null) {
+      paths.push(path);
+      return;
+    }
+
+    if (root.left) {
+      dfs(root.left, `${path}->${root.left.val.toString()}`);
+    }
+
+    if (root.right) {
+      dfs(root.right, `${path}->${root.right.val.toString()}`);
+    }
+  };
+
+  if (root === null) {
+    return paths;
+  }
+
+  dfs(root, root.val.toString());
+  return paths;
+}
