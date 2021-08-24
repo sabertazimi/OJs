90caec87640a369ff0682871b49194feef04e842
sabertazimi <sabertazimi@gmail.com>
2021-08-24 22:30:50 +0800

    tree(leetcode-0100): same tree comparsion

---
+++ leetCode/0100.test.ts
@@ -0,0 +1,25 @@
+import solution from './0100';
+import { arrayToTree } from './Tree';
+
+/**
+ * @author sabertazimi
+ * @license MIT
+ * @level easy
+ * @description same-tree
+ */
+describe('LeetCode [0100]', () => {
+  test('should AC', () => {
+    expect(solution(arrayToTree([1, 2]), arrayToTree([1, 0, 2]))).toStrictEqual(
+      false
+    );
+    expect(solution(arrayToTree([1, 0, 2]), arrayToTree([1, 2]))).toStrictEqual(
+      false
+    );
+    expect(
+      solution(arrayToTree([1, 2, 1]), arrayToTree([1, 1, 2]))
+    ).toStrictEqual(false);
+    expect(
+      solution(arrayToTree([1, 2, 3]), arrayToTree([1, 2, 3]))
+    ).toStrictEqual(true);
+  });
+});
---
+++ leetCode/0100.ts
@@ -0,0 +1,17 @@
+import { TreeNode } from './Tree';
+
+export default function isSameTree<T>(
+  p: TreeNode<T> | null,
+  q: TreeNode<T> | null
+): boolean {
+  if (p === null && q !== null) return false;
+  if (p !== null && q === null) return false;
+  if (p !== null && q !== null) {
+    return (
+      p.val === q.val &&
+      isSameTree(p.left, q.left) &&
+      isSameTree(p.right, q.right)
+    );
+  }
+  return p === null && q === null;
+}
