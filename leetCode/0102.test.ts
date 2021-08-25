525e11445cc1bac3a09834262c3e7de931e31a09
sabertazimi <sabertazimi@gmail.com>
2021-08-25 14:57:55 +0800

    tree(leetcode-0102): binary tree level order traversal

---
+++ leetCode/0102.test.ts
@@ -0,0 +1,28 @@
+import solution from './0102';
+import { arrayToTree } from './Tree';
+
+/**
+ * @author sabertazimi
+ * @license MIT
+ * @level medium
+ * @description binary-tree-level-order-traversal
+ * Given the root of a binary tree,
+ * return the level order traversal of its nodes' values.
+ * (i.e., from left to right, level by level).
+ */
+describe('LeetCode [0102]', () => {
+  test('should AC', () => {
+    expect(solution(arrayToTree([]))).toStrictEqual([]);
+    expect(solution(arrayToTree([1]))).toStrictEqual([[1]]);
+    expect(solution(arrayToTree([3, 9, 20, 0, 0, 15, 7]))).toStrictEqual([
+      [3],
+      [9, 20],
+      [15, 7],
+    ]);
+    expect(solution(arrayToTree([3, 9, 20, 4, 5, 15, 7]))).toStrictEqual([
+      [3],
+      [9, 20],
+      [4, 5, 15, 7],
+    ]);
+  });
+});
