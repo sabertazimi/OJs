993bb9d5576dcec33fddcf74dc0c353826a427d1
sabertazimi <sabertazimi@gmail.com>
2021-08-25 21:01:54 +0800

    math(leetcode-0119): pascals triangle ii

---
+++ leetCode/0119.test.ts
@@ -0,0 +1,18 @@
+import solution from './0119';
+
+/**
+ * @author sabertazimi
+ * @license MIT
+ * @level easy
+ * @description pascals-triangle-ii
+ * Given an integer rowIndex,
+ * return the row Index (0-indexed) row of the Pascal's triangle.
+ */
+describe('LeetCode [0119]', () => {
+  test('should AC', () => {
+    expect(solution(0)).toStrictEqual([1]);
+    expect(solution(1)).toStrictEqual([1, 1]);
+    expect(solution(3)).toStrictEqual([1, 3, 3, 1]);
+    expect(solution(4)).toStrictEqual([1, 4, 6, 4, 1]);
+  });
+});
