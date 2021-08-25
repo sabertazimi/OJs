077602bd0ec71df6aba20fcbc1661563a0593564
sabertazimi <sabertazimi@gmail.com>
2021-08-25 14:26:42 +0800

    tree(leetcode-0101): symmetric tree

---
+++ leetCode/0101.test.ts
@@ -0,0 +1,19 @@
+import solution from './0101';
+import { arrayToTree } from './Tree';
+
+/**
+ * @author sabertazimi
+ * @license MIT
+ * @level easy
+ * @description symmetric-tree
+ * Given the root of a binary tree,
+ * check whether it is a mirror of itself.
+ */
+describe('LeetCode [0101]', () => {
+  test('should AC', () => {
+    expect(solution(arrayToTree([]))).toStrictEqual(true);
+    expect(solution(arrayToTree([1, 2, 2, 0, 3, 0, 3]))).toStrictEqual(false);
+    expect(solution(arrayToTree([1, 2, 2, 3, 5, 4, 3]))).toStrictEqual(false);
+    expect(solution(arrayToTree([1, 2, 2, 3, 4, 4, 3]))).toStrictEqual(true);
+  });
+});
