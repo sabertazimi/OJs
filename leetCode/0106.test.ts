c155603cc865d5073489ae2b0d55fe9cdf9e26e3
sabertazimi <sabertazimi@gmail.com>
2021-08-25 15:48:16 +0800

    tree(leetcode-0106): construct tree from inorder and postorder traversal

---
+++ leetCode/0106.test.ts
@@ -0,0 +1,21 @@
+import solution from './0106';
+import { arrayToTree } from './Tree';
+
+/**
+ * @author sabertazimi
+ * @license MIT
+ * @level medium
+ * @description construct-binary-tree-from-inorder-and-postorder-traversal
+ * Given two integer arrays inorder and postorder
+ * where inorder is the inorder traversal of a binary tree
+ * and postorder is the postorder traversal of the same tree,
+ * construct and return the binary tree.
+ */
+describe('LeetCode [0106]', () => {
+  test('should AC', () => {
+    expect(solution([-1], [-1])).toStrictEqual(arrayToTree([-1]));
+    expect(solution([9, 3, 15, 20, 7], [9, 15, 7, 20, 3])).toStrictEqual(
+      arrayToTree([3, 9, 20, 0, 0, 15, 7])
+    );
+  });
+});
