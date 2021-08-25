a1c072e583036c03ff1cb1d7659a3b17a7f58cee
sabertazimi <sabertazimi@gmail.com>
2021-08-25 20:36:46 +0800

    refactor(leetcode-List/Tree): format code

--- leetCode/List.ts
+++ leetCode/List.ts
@@ -1,6 +1,7 @@
 class ListNode<T> {
   val: T;
   next: ListNode<T> | null;
+
   constructor(val: T, next?: ListNode<T> | null) {
     this.val = val;
     this.next = next === undefined ? null : next;
--- leetCode/Tree.ts
+++ leetCode/Tree.ts
@@ -2,6 +2,7 @@ class TreeNode<T> {
   val: T;
   left: TreeNode<T> | null;
   right: TreeNode<T> | null;
+
   constructor(val: T) {
     this.val = val;
     this.left = null;
