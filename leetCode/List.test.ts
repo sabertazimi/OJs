37bb2ea81fcf3f2f753cdec058d31a05d74e60d7
sabertazimi <sabertazimi@gmail.com>
2021-08-25 17:53:02 +0800

    test(leetcode-List/Tree): add more testing

--- leetCode/List.test.ts
+++ leetCode/List.test.ts
@@ -1,6 +1,47 @@
-import { arrayToList, printList } from './List';
+import { arrayToList, ListNode, printList } from './List';
 
 describe('List', () => {
+  test('should construct `null` list from empty array', () => {
+    const list = arrayToList([]);
+    expect(list).toStrictEqual(null);
+  });
+
+  test('should construct from [number array] correctly', () => {
+    const list = arrayToList([1, 2, 3, 4, 5]);
+    expect(list).toBeTruthy();
+    expect(list?.val).toStrictEqual(1);
+    expect(list?.next?.val).toStrictEqual(2);
+    expect(list?.next?.next?.val).toStrictEqual(3);
+    expect(list?.next?.next?.next?.val).toStrictEqual(4);
+    expect(list?.next?.next?.next?.next?.val).toStrictEqual(5);
+  });
+
+  test('should construct from [string array] correctly', () => {
+    const list = arrayToList(['1', '2', '3', '4', '5']);
+    expect(list).toBeTruthy();
+    expect(list?.val).toStrictEqual('1');
+    expect(list?.next?.val).toStrictEqual('2');
+    expect(list?.next?.next?.val).toStrictEqual('3');
+    expect(list?.next?.next?.next?.val).toStrictEqual('4');
+    expect(list?.next?.next?.next?.next?.val).toStrictEqual('5');
+  });
+
+  test('should construct from [class array] correctly', () => {
+    const list = arrayToList([
+      new ListNode(1),
+      new ListNode(2),
+      new ListNode(3),
+      new ListNode(4),
+      new ListNode(5),
+    ]);
+    expect(list).toBeTruthy();
+    expect(list?.val).toStrictEqual(new ListNode(1));
+    expect(list?.next?.val).toStrictEqual(new ListNode(2));
+    expect(list?.next?.next?.val).toStrictEqual(new ListNode(3));
+    expect(list?.next?.next?.next?.val).toStrictEqual(new ListNode(4));
+    expect(list?.next?.next?.next?.next?.val).toStrictEqual(new ListNode(5));
+  });
+
   test('should print correctly', () => {
     const mockConsoleInfo = jest
       .spyOn(console, 'info')
--- leetCode/Tree.test.ts
+++ leetCode/Tree.test.ts
@@ -1,20 +1,90 @@
-import { ListNode } from './List';
-import { arrayToTree } from './Tree';
+import { arrayToTree, traversal, TreeNode } from './Tree';
 
 describe('Tree', () => {
+  test('should construct `null` tree from empty array', () => {
+    const tree = arrayToTree([]);
+    expect(tree).toStrictEqual(null);
+  });
+
+  test('should construct from [number array] correctly', () => {
+    const tree = arrayToTree([1, 2, 3, null, null, 5, 6]);
+    expect(tree).toBeTruthy();
+    expect(tree?.val).toStrictEqual(1);
+    expect(tree?.left?.val).toStrictEqual(2);
+    expect(tree?.right?.val).toStrictEqual(3);
+    expect(tree?.left?.left).toStrictEqual(null);
+    expect(tree?.left?.right).toStrictEqual(null);
+    expect(tree?.right?.left?.val).toStrictEqual(5);
+    expect(tree?.right?.right?.val).toStrictEqual(6);
+  });
+
   test('should construct from [string array] correctly', () => {
-    arrayToTree(['1', '2', '3', 'null', 'null', '5', '6']);
+    const tree = arrayToTree(['1', '2', '3', null, null, '5', '6']);
+    expect(tree).toBeTruthy();
+    expect(tree?.val).toStrictEqual('1');
+    expect(tree?.left?.val).toStrictEqual('2');
+    expect(tree?.right?.val).toStrictEqual('3');
+    expect(tree?.left?.left).toStrictEqual(null);
+    expect(tree?.left?.right).toStrictEqual(null);
+    expect(tree?.right?.left?.val).toStrictEqual('5');
+    expect(tree?.right?.right?.val).toStrictEqual('6');
   });
 
   test('should construct from [class array] correctly', () => {
-    arrayToTree([
-      new ListNode(1),
-      new ListNode(2),
-      new ListNode(3),
+    const tree = arrayToTree([
+      new TreeNode(1),
+      new TreeNode(2),
+      new TreeNode(3),
       null,
       null,
-      new ListNode(5),
-      new ListNode(6),
+      new TreeNode(5),
+      new TreeNode(6),
     ]);
+    expect(tree).toBeTruthy();
+    expect(tree?.val).toStrictEqual(new TreeNode(1));
+    expect(tree?.left?.val).toStrictEqual(new TreeNode(2));
+    expect(tree?.right?.val).toStrictEqual(new TreeNode(3));
+    expect(tree?.left?.left).toStrictEqual(null);
+    expect(tree?.left?.right).toStrictEqual(null);
+    expect(tree?.right?.left?.val).toStrictEqual(new TreeNode(5));
+    expect(tree?.right?.right?.val).toStrictEqual(new TreeNode(6));
+  });
+
+  test('should perform preorder traversal correctly', () => {
+    const tree = arrayToTree([1, 2, 3, null, null, 5, 6]);
+    const nodes: number[] = [];
+    traversal(
+      tree,
+      node => {
+        nodes.push(node);
+      },
+      'pre'
+    );
+    expect(tree).toBeTruthy();
+    expect(nodes).toStrictEqual([1, 2, 3, 5, 6]);
+  });
+
+  test('should perform inorder traversal correctly', () => {
+    const tree = arrayToTree([1, 2, 3, null, null, 5, 6]);
+    const nodes: number[] = [];
+    traversal(tree, node => {
+      nodes.push(node);
+    });
+    expect(tree).toBeTruthy();
+    expect(nodes).toStrictEqual([2, 1, 5, 3, 6]);
+  });
+
+  test('should perform postorder traversal correctly', () => {
+    const tree = arrayToTree([1, 2, 3, null, null, 5, 6]);
+    const nodes: number[] = [];
+    traversal(
+      tree,
+      node => {
+        nodes.push(node);
+      },
+      'post'
+    );
+    expect(tree).toBeTruthy();
+    expect(nodes).toStrictEqual([2, 5, 6, 3, 1]);
   });
 });
