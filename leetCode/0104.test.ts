277d1c1eeb19cb82d75d65c5832e6184cb85aaf7
sabertazimi <sabertazimi@gmail.com>
2021-08-25 15:22:42 +0800

    tree(leetcode-0104): maximum depth of binary tree

---
+++ leetCode/0104.test.ts
@@ -0,0 +1,19 @@
+import solution from './0104';
+import { arrayToTree } from './Tree';
+
+/**
+ * @author sabertazimi
+ * @license MIT
+ * @level easy
+ * @description maximum-depth-of-binary-tree
+ * Given the root of a binary tree, return its maximum depth.
+ * A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ */
+describe('LeetCode [0104]', () => {
+  test('should AC', () => {
+    expect(solution(arrayToTree([]))).toStrictEqual(0);
+    expect(solution(arrayToTree([1]))).toStrictEqual(1);
+    expect(solution(arrayToTree([1, 0, 2]))).toStrictEqual(2);
+    expect(solution(arrayToTree([3, 9, 20, 0, 0, 15, 7]))).toStrictEqual(3);
+  });
+});
