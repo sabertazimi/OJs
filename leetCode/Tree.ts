8662ededb3209ecff7b0860a13fa33eebdd7a958
sabertazimi <sabertazimi@gmail.com>
2021-08-25 17:31:36 +0800

    fix(leetcode-Tree): rectify tree nodes array type

--- leetCode/Tree.ts
+++ leetCode/Tree.ts
@@ -10,22 +10,26 @@ class TreeNode<T> {
 }
 
 /**
- * @param nums Tree node array (0 present `null` node)
+ * @param nodes Tree node array (include the `null` node)
  * @returns Root of binary tree
  */
-const arrayToTree = <T>(nums: T[]): TreeNode<T> | null => {
-  if (nums.length === 0) return null;
-  const root = insertToTree(nums, 0);
+const arrayToTree = <T>(nodes: (T | null)[]): TreeNode<T> | null => {
+  if (nodes.length === 0) return null;
+  const root = insertToTree(nodes, 0);
   return root;
 };
 
-const insertToTree = <T>(nums: T[], index: number): TreeNode<T> | null => {
+const insertToTree = <T>(
+  nodes: (T | null)[],
+  index: number
+): TreeNode<T> | null => {
   let root: TreeNode<T> | null = null;
+  const node = nodes[index];
 
-  if (index < nums.length && nums[index]) {
-    root = new TreeNode(nums[index]);
-    root.left = insertToTree(nums, 2 * index + 1);
-    root.right = insertToTree(nums, 2 * index + 2);
+  if (node !== null && index < nodes.length) {
+    root = new TreeNode(node);
+    root.left = insertToTree(nodes, 2 * index + 1);
+    root.right = insertToTree(nodes, 2 * index + 2);
   }
 
   return root;
