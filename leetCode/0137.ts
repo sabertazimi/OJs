10f037edf350ce4b8b9a0037610037cc029573fd
sabertazimi <sabertazimi@gmail.com>
2021-08-26 15:27:22 +0800

    math(leetcode-0137): single number ii

---
+++ leetCode/0137.ts
@@ -0,0 +1,17 @@
+export default function singleNumber(nums: number[]): number {
+  let res = 0;
+
+  for (let i = 0; i < 32; ++i) {
+    let total = 0;
+
+    for (const num of nums) {
+      total += (num >> i) & 1;
+    }
+
+    if (total % 3 !== 0) {
+      res |= 1 << i;
+    }
+  }
+
+  return res;
+}
