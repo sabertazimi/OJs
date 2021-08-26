2bc474f17dec0ab8b79ed17d01725d71bcba424e
sabertazimi <sabertazimi@gmail.com>
2021-08-26 15:06:48 +0800

    math(leetcode-0136): single number (bitwise operator)

---
+++ leetCode/0136.test.ts
@@ -0,0 +1,20 @@
+import solution from './0136';
+
+/**
+ * @author sabertazimi
+ * @license MIT
+ * @level easy
+ * @description single-number
+ * Given a non-empty array of integers nums,
+ * every element appears twice except for one.
+ * Find that single one.
+ * You must implement a solution with a linear runtime complexity
+ * and use only constant extra space.
+ */
+describe('LeetCode [0136]', () => {
+  test('should AC', () => {
+    expect(solution([1])).toStrictEqual(1);
+    expect(solution([2, 2, 1])).toStrictEqual(1);
+    expect(solution([4, 1, 2, 1, 2])).toStrictEqual(4);
+  });
+});
