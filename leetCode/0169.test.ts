2fe579915242e395b62ffd169c4715315989a024
sabertazimi <sabertazimi@gmail.com>
2021-08-26 18:16:39 +0800

    array(leetcode-0169): majority element

---
+++ leetCode/0169.test.ts
@@ -0,0 +1,17 @@
+import solution from './0169';
+
+/**
+ * @author sabertazimi
+ * @license MIT
+ * @level easy
+ * @description majority-element
+ * Given an array nums of size n, return the majority element.
+ * The majority element is the element that appears more than ⌊n / 2⌋ times.
+ * You may assume that the majority element always exists in the array.
+ */
+describe('LeetCode [0169]', () => {
+  test('should AC', () => {
+    expect(solution([3, 2, 3])).toStrictEqual(3);
+    expect(solution([2, 2, 1, 1, 1, 2, 2])).toStrictEqual(2);
+  });
+});
