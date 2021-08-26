4219be578b74f883568f2087d85d5bee530f5b92
sabertazimi <sabertazimi@gmail.com>
2021-08-26 15:49:29 +0800

    list(leetcode-0142): linked cycle ii (set map)

---
+++ leetCode/0142.test.ts
@@ -0,0 +1,25 @@
+import solution from './0142';
+import { arrayToList } from './List';
+
+/**
+ * @author sabertazimi
+ * @license MIT
+ * @level medium
+ * @description linked-list-cycle
+ * Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+ * There is a cycle in a linked list if there is some node in the list
+ * that can be reached again by continuously following the next pointer.
+ * Internally, pos is used to denote the index of the node that tail's next pointer is connected to.
+ * Note that pos is not passed as a parameter.
+ */
+describe('LeetCode [0142]', () => {
+  test('should AC', () => {
+    expect(solution(arrayToList([]))).toStrictEqual(null);
+    expect(solution(arrayToList([1]))).toStrictEqual(null);
+    expect(solution(arrayToList([1, 2, 3, 4, 5]))).toStrictEqual(null);
+
+    const list1 = arrayToList([1, 2]);
+    if (list1?.next) list1.next.next = list1;
+    expect(solution(list1)).toStrictEqual(list1);
+  });
+});
