f4bbea3023530178d914a3a24275a5cdca14983f
sabertazimi <sabertazimi@gmail.com>
2021-08-26 22:54:32 +0800

    math(leetcode-0204): count primes

---
+++ leetCode/0204.test.ts
@@ -0,0 +1,16 @@
+import solution from './0204';
+
+/**
+ * @author sabertazimi
+ * @license MIT
+ * @level easy
+ * @description count-primes
+ * Count the number of prime numbers less than a non-negative number, n.
+ */
+describe('LeetCode [0204]', () => {
+  test('should AC', () => {
+    expect(solution(0)).toStrictEqual(0);
+    expect(solution(1)).toStrictEqual(0);
+    expect(solution(10)).toStrictEqual(4);
+  });
+});
