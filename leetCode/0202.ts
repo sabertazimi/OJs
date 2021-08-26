9df015b9340e27c7a07bba7971f506dca9f91fa4
sabertazimi <sabertazimi@gmail.com>
2021-08-26 22:10:02 +0800

    math(leetcode-0202): happy number

---
+++ leetCode/0202.ts
@@ -0,0 +1,26 @@
+export default function isHappy(n: number): boolean {
+  let slow = n;
+  let fast = n;
+
+  do {
+    slow = squareNum(slow);
+    fast = squareNum(squareNum(fast));
+  } while (slow !== fast);
+
+  if (slow === 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const squareNum = (n: number): number => {
+  let sum = 0;
+
+  while (n) {
+    sum += (n % 10) * (n % 10);
+    n = Math.floor(n / 10);
+  }
+
+  return sum;
+};
