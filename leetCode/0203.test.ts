b367fd4e537883f6ca9b911228a935eadd16d702
sabertazimi <sabertazimi@gmail.com>
2021-08-26 22:43:58 +0800

    list(leetcode-0203): remove linked list elements

---
+++ leetCode/0203.test.ts
@@ -0,0 +1,23 @@
+import solution from './0203';
+import { arrayToList } from './List';
+
+/**
+ * @author sabertazimi
+ * @license MIT
+ * @level easy
+ * @description remove-linked-list-elements
+ * Given the head of a linked list and an integer val,
+ * remove all the nodes of the linked list that has Node.val == val,
+ * and return the new head.
+ */
+describe('LeetCode [0203]', () => {
+  test('should AC', () => {
+    expect(solution(arrayToList([]), 1)).toStrictEqual(arrayToList([]));
+    expect(solution(arrayToList([7, 7, 7, 7]), 7)).toStrictEqual(
+      arrayToList([])
+    );
+    expect(solution(arrayToList([1, 2, 6, 3, 4, 5, 6]), 6)).toStrictEqual(
+      arrayToList([1, 2, 3, 4, 5])
+    );
+  });
+});
