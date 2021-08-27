b854cc406a94d5989d89c66867d46c491fdff81a
sabertazimi <sabertazimi@gmail.com>
2021-08-28 05:22:25 +0800

    tree(leetcode-0235): lowest common ancestor of a binary search tree

---
+++ leetCode/0235.test.ts
@@ -0,0 +1,52 @@
+import solution from './0235';
+import { arrayToTree, TreeNode } from './Tree';
+
+/**
+ * @author sabertazimi
+ * @license MIT
+ * @level easy
+ * @description lowest-common-ancestor-of-a-binary-search-tree
+ * Given a binary search tree (BST),
+ * find the lowest common ancestor (LCA) of two given nodes in the BST.
+ * According to the definition of LCA on Wikipedia:
+ * The lowest common ancestor is defined between two nodes p and q as the lowest node in T
+ * that has both p and q as descendants (where we allow a node to be a descendant of itself).
+ */
+describe('LeetCode [0235]', () => {
+  test('should AC', () => {
+    const tree = arrayToTree([
+      6,
+      2,
+      8,
+      0,
+      4,
+      7,
+      9,
+      null,
+      null,
+      3,
+      5,
+    ]) as TreeNode<number>;
+    expect(
+      solution(
+        tree,
+        tree?.left as TreeNode<number>,
+        tree?.right as TreeNode<number>
+      )
+    ).toStrictEqual(tree);
+    expect(
+      solution(
+        tree,
+        tree?.left as TreeNode<number>,
+        tree?.left?.right as TreeNode<number>
+      )
+    ).toStrictEqual(tree?.left);
+    expect(
+      solution(
+        tree,
+        tree?.right as TreeNode<number>,
+        tree?.right?.right as TreeNode<number>
+      )
+    ).toStrictEqual(tree?.right);
+  });
+});
