0f6b12903d5950c71315b92d02716e8d428b02cd
sabertazimi <sabertazimi@gmail.com>
2021-08-28 21:47:54 +0800

    tree(Segment Tree): implement segment tree for range update/query

--- leetCode/Tree.test.ts
+++ leetCode/Tree.test.ts
@@ -1,4 +1,4 @@
-import { arrayToTree, traversal, TreeNode } from './Tree';
+import { arrayToTree, traversal, TreeNode, SegmentTree } from './Tree';
 
 describe('Tree', () => {
   test('should construct `null` tree from empty array', () => {
@@ -86,5 +86,70 @@ describe('Tree', () => {
     );
     expect(tree).toBeTruthy();
     expect(nodes).toStrictEqual([2, 5, 6, 3, 1]);
+  });
+});
+
+describe('SegmentTree', () => {
+  test('should throw error when constructing from empty array', () => {
+    expect(() => new SegmentTree([], jest.fn(), 0)).toThrow(
+      Error('Data is empty')
+    );
+  });
+
+  test('should construct from [number array] correctly', () => {
+    const segmentTree = new SegmentTree([1, 2, 3, 4, 5], jest.fn(), 0);
+    expect(segmentTree).toBeTruthy();
+    expect(segmentTree.get(0)).toStrictEqual(1);
+    expect(segmentTree.get(1)).toStrictEqual(2);
+    expect(segmentTree.get(2)).toStrictEqual(3);
+    expect(segmentTree.get(3)).toStrictEqual(4);
+    expect(segmentTree.get(4)).toStrictEqual(5);
+  });
+
+  test('should construct from [string array] correctly', () => {
+    const segmentTree = new SegmentTree(['1', '2', '3'], jest.fn(), '0');
+    expect(segmentTree).toBeTruthy();
+    expect(segmentTree.get(0)).toStrictEqual('1');
+    expect(segmentTree.get(1)).toStrictEqual('2');
+    expect(segmentTree.get(2)).toStrictEqual('3');
+  });
+
+  test('should construct from [class array] correctly', () => {
+    const segmentTree = new SegmentTree(
+      [new TreeNode(1), new TreeNode(2), new TreeNode(3)],
+      jest.fn(),
+      null
+    );
+    expect(segmentTree).toBeTruthy();
+    expect(segmentTree.get(0)).toStrictEqual(new TreeNode(1));
+    expect(segmentTree.get(1)).toStrictEqual(new TreeNode(2));
+    expect(segmentTree.get(2)).toStrictEqual(new TreeNode(3));
+  });
+
+  test('should update value correctly', () => {
+    const segmentTree = new SegmentTree([1, 2, 3, 4, 5], jest.fn(), 0);
+    segmentTree.set(0, 5);
+    segmentTree.set(1, 4);
+    segmentTree.set(2, 3);
+    segmentTree.set(3, 2);
+    segmentTree.set(4, 1);
+    expect(segmentTree.get(0)).toStrictEqual(5);
+    expect(segmentTree.get(1)).toStrictEqual(4);
+    expect(segmentTree.get(2)).toStrictEqual(3);
+    expect(segmentTree.get(3)).toStrictEqual(2);
+    expect(segmentTree.get(4)).toStrictEqual(1);
+  });
+
+  test('should query range sum correctly', () => {
+    const segmentTree = new SegmentTree(
+      [1, 2, 3, 4, 5],
+      (a: number, b: number) => a + b,
+      0
+    );
+    expect(segmentTree.query(0, 4)).toStrictEqual(15);
+    expect(segmentTree.query(0, 3)).toStrictEqual(10);
+    expect(segmentTree.query(1, 3)).toStrictEqual(9);
+    expect(segmentTree.query(2, 3)).toStrictEqual(7);
+    expect(segmentTree.query(2, 2)).toStrictEqual(3);
   });
 });
--- leetCode/Tree.ts
+++ leetCode/Tree.ts
@@ -78,4 +78,123 @@ const traversal = <T>(
   }
 };
 
-export { TreeNode, arrayToTree, traversal };
+class SegmentTree<T> {
+  private readonly data: T[];
+  private readonly tree: T[];
+  private readonly fn: (left: T, right: T) => T;
+  private readonly init: T;
+
+  constructor(data: T[], fn: (left: T, right: T) => T, init: T) {
+    if (data.length === 0) throw new Error('Data is empty');
+    this.data = data;
+    this.fn = fn;
+    this.init = init;
+    this.tree = new Array(data.length * 4).fill(init);
+    this.build(0, 0, data.length - 1);
+  }
+
+  public get(index: number): T {
+    return this.tree[this.getIndex(0, 0, this.data.length - 1, index)];
+  }
+
+  public set(index: number, value: T): void {
+    this.update(index, value);
+  }
+
+  public query(left: number, right: number): T {
+    return this.queryHelper(0, 0, this.data.length - 1, left, right);
+  }
+
+  private build(treeIndex: number, left: number, right: number): void {
+    if (left === right) {
+      this.tree[treeIndex] = this.data[left];
+      return;
+    }
+
+    const mid = left + Math.floor((right - left) / 2);
+    this.build(treeIndex * 2 + 1, left, mid);
+    this.build(treeIndex * 2 + 2, mid + 1, right);
+    this.tree[treeIndex] = this.fn(
+      this.tree[treeIndex * 2 + 1],
+      this.tree[treeIndex * 2 + 2]
+    );
+  }
+
+  private getIndex(
+    treeIndex: number,
+    left: number,
+    right: number,
+    index: number
+  ): number {
+    if (left === right) {
+      return treeIndex;
+    }
+    const mid = left + Math.floor((right - left) / 2);
+    if (index <= mid) {
+      return this.getIndex(treeIndex * 2 + 1, left, mid, index);
+    } else {
+      return this.getIndex(treeIndex * 2 + 2, mid + 1, right, index);
+    }
+  }
+
+  private update(index: number, value: T): void {
+    this.updateHelper(0, 0, this.data.length - 1, index, value);
+  }
+
+  private updateHelper(
+    treeIndex: number,
+    left: number,
+    right: number,
+    index: number,
+    value: T
+  ): void {
+    if (left === right) {
+      this.tree[treeIndex] = value;
+      return;
+    }
+
+    const mid = left + Math.floor((right - left) / 2);
+    if (index <= mid)
+      this.updateHelper(treeIndex * 2 + 1, left, mid, index, value);
+    else this.updateHelper(treeIndex * 2 + 2, mid + 1, right, index, value);
+    this.tree[treeIndex] = this.fn(
+      this.tree[treeIndex * 2 + 1],
+      this.tree[treeIndex * 2 + 2]
+    );
+  }
+
+  private queryHelper(
+    treeIndex: number,
+    left: number,
+    right: number,
+    queryLeft: number,
+    queryRight: number
+  ): T {
+    if (queryLeft <= left && right <= queryRight) {
+      return this.tree[treeIndex];
+    }
+
+    if (queryRight < left || right < queryLeft) {
+      return this.init;
+    }
+
+    const mid = left + Math.floor((right - left) / 2);
+    const leftResult = this.queryHelper(
+      treeIndex * 2 + 1,
+      left,
+      mid,
+      queryLeft,
+      queryRight
+    );
+    const rightResult = this.queryHelper(
+      treeIndex * 2 + 2,
+      mid + 1,
+      right,
+      queryLeft,
+      queryRight
+    );
+    return this.fn(leftResult, rightResult);
+  }
+}
+
+export { TreeNode, SegmentTree, arrayToTree, traversal };
