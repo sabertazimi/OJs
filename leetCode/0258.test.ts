f38770c3cc4bc21167a416ca8e8c94eb6cdbfea2
sabertazimi <sabertazimi@gmail.com>
2021-08-28 19:18:31 +0800

    math(leetcode-0258): add digits

---
+++ leetCode/0258.test.ts
@@ -0,0 +1,18 @@
+import solution from './0258';
+
+/**
+ * @author sabertazimi
+ * @license MIT
+ * @level easy
+ * @description add-digits
+ * Given an integer num,
+ * repeatedly add all its digits until the result has only one digit,
+ * and return it.
+ */
+describe('LeetCode [0258]', () => {
+  test('should AC', () => {
+    expect(solution(0)).toStrictEqual(0);
+    expect(solution(9)).toStrictEqual(9);
+    expect(solution(38)).toStrictEqual(2);
+  });
+});
