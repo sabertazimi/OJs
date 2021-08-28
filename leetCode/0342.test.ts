792c17e669e82c89bc5b43d9e5496ae69c744cf7
sabertazimi <sabertazimi@gmail.com>
2021-08-29 01:16:30 +0800

    math(leetcode-0342): power of four

---
+++ leetCode/0342.test.ts
@@ -0,0 +1,18 @@
+import solution from './0342';
+
+/**
+ * @author sabertazimi
+ * @license MIT
+ * @level easy
+ * @description power-of-four
+ * Given an integer n, return true if it is a power of four. Otherwise, return false.
+ * An integer n is a power of four, if there exists an integer x such that n == 4x.
+ */
+describe('LeetCode [0342]', () => {
+  test('should AC', () => {
+    expect(solution(0)).toStrictEqual(false);
+    expect(solution(1)).toStrictEqual(true);
+    expect(solution(5)).toStrictEqual(false);
+    expect(solution(16)).toStrictEqual(true);
+  });
+});
--- leetCode/342_power_of_four.cc
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    bool isPowerOfFour(int num) {
-        // num & (num - 1) => power of 2
-        // (num - 1) % 3 => 1 on odd bit
-        return num > 0 && (num & (num - 1)) == 0 && (num - 1) % 3 == 0;
-    }
-};
