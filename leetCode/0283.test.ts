3174e4be1935bc6a43e7dbb0919d5db998d1af83
sabertazimi <sabertazimi@gmail.com>
2021-08-28 20:06:42 +0800

    array(leetcode-0283): move zeros

---
+++ leetCode/0283.test.ts
@@ -0,0 +1,17 @@
+import solution from './0283';
+
+/**
+ * @author sabertazimi
+ * @license MIT
+ * @level easy
+ * @description move-zeros
+ * Given an integer array nums,
+ * move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+ */
+describe('LeetCode [0283]', () => {
+  test('should AC', () => {
+    expect(solution([0])).toStrictEqual([0]);
+    expect(solution([0, 0, 1])).toStrictEqual([1, 0, 0]);
+    expect(solution([0, 1, 0, 3, 12])).toStrictEqual([1, 3, 12, 0, 0]);
+  });
+});
