f839b2bfc40a2390f8a704068a3a3582720d66da
sabertazimi <sabertazimi@gmail.com>
2021-08-28 20:16:12 +0800

    hash(leetcode-0290): word patterns

---
+++ leetCode/0290.test.ts
@@ -0,0 +1,21 @@
+import solution from './0290';
+
+/**
+ * @author sabertazimi
+ * @license MIT
+ * @level easy
+ * @description word-pattern
+ * Given a pattern and a string s,
+ * find if s follows the same pattern.
+ * Here follow means a full match,
+ * such that there is a bijection between a letter in pattern and a non-empty word in s.
+ */
+describe('LeetCode [0290]', () => {
+  test('should AC', () => {
+    expect(solution('aaaa', 'dog dog dog')).toStrictEqual(false);
+    expect(solution('aaaa', 'dog cat cat dog')).toStrictEqual(false);
+    expect(solution('abba', 'dog dog dog dog')).toStrictEqual(false);
+    expect(solution('abba', 'dog cat cat fish')).toStrictEqual(false);
+    expect(solution('abba', 'dog cat cat dog')).toStrictEqual(true);
+  });
+});
