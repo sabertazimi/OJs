4007f1cf396ed7b70d6a21b2a1bd17be482ce228
sabertazimi <sabertazimi@gmail.com>
2021-08-29 01:59:42 +0800

    math(leetcode-0367): valid perfect square

---
+++ leetCode/0367.test.ts
@@ -0,0 +1,17 @@
+import solution from './0367';
+
+/**
+ * @author sabertazimi
+ * @license MIT
+ * @level easy
+ * @description valid-perfect-square
+ * Given a positive integer num,
+ * write a function which returns True if num is a perfect square else False.
+ * Follow up: Do not use any built-in library function such as sqrt.
+ */
+describe('LeetCode [0367]', () => {
+  test('should AC', () => {
+    expect(solution(14)).toStrictEqual(false);
+    expect(solution(16)).toStrictEqual(true);
+  });
+});
---
+++ leetCode/0367.ts
@@ -0,0 +1,7 @@
+export default function isPerfectSquare(num: number): boolean {
+  for (let i = 1; i * i <= num; i++) {
+    if (i * i === num) return true;
+  }
+
+  return false;
+}
