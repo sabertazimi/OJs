03cd73a9ad16fb382323648a67d563e8182376ee
sabertazimi <sabertazimi@gmail.com>
2021-08-28 19:23:16 +0800

    math(leetcode-0263): ugly number

---
+++ leetCode/0263.test.ts
@@ -0,0 +1,20 @@
+import solution from './0263';
+
+/**
+ * @author sabertazimi
+ * @license MIT
+ * @level easy
+ * @description ugly-number
+ * An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+ * Given an integer n, return true if n is an ugly number.
+ */
+describe('LeetCode [0263]', () => {
+  test('should AC', () => {
+    expect(solution(0)).toStrictEqual(false);
+    expect(solution(1)).toStrictEqual(true);
+    expect(solution(6)).toStrictEqual(true);
+    expect(solution(8)).toStrictEqual(true);
+    expect(solution(14)).toStrictEqual(false);
+    expect(solution(25)).toStrictEqual(true);
+  });
+});
