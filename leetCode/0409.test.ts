1b48a1c2525ac4758fec727eeb0284f82215feed
sabertazimi <sabertazimi@gmail.com>
2021-08-29 18:46:50 +0800

    string(leetcode-0409): longest palindrome

---
+++ leetCode/0409.test.ts
@@ -0,0 +1,19 @@
+import solution from './0409';
+
+/**
+ * @author sabertazimi
+ * @license MIT
+ * @level easy
+ * @description longest-palindrome
+ * Given a string s which consists of lowercase or uppercase letters,
+ * return the length of the longest palindrome that can be built with those letters.
+ * Letters are case sensitive,
+ * for example, "Aa" is not considered a palindrome here.
+ */
+describe('LeetCode [0409]', () => {
+  test('should AC', () => {
+    expect(solution('a')).toStrictEqual(1);
+    expect(solution('bb')).toStrictEqual(2);
+    expect(solution('abccccdd')).toStrictEqual(7);
+  });
+});
