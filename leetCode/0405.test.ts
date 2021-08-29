0fbfc54716de719e5c49ed398c93a383c8a3e374
sabertazimi <sabertazimi@gmail.com>
2021-08-29 18:41:30 +0800

    math(leetcode-0405): convert a number to hexadecimal

---
+++ leetCode/0405.test.ts
@@ -0,0 +1,19 @@
+import solution from './0405';
+
+/**
+ * @author sabertazimi
+ * @license MIT
+ * @level easy
+ * @description convert-a-number-to-hexadecimal
+ * Given an integer num, return a string representing its hexadecimal representation.
+ * For negative integers, two’s complement method is used.
+ * All the letters in the answer string should be lowercase characters,
+ * and there should not be any leading zeros in the answer except for the zero itself.
+ */
+describe('LeetCode [0405]', () => {
+  test('should AC', () => {
+    expect(solution(0)).toStrictEqual('0');
+    expect(solution(26)).toStrictEqual('1a');
+    expect(solution(-1)).toStrictEqual('ffffffff');
+  });
+});
---
+++ leetCode/0405.ts
@@ -0,0 +1,13 @@
+export default function toHex(num: number): string {
+  if (num === 0) return '0';
+
+  let res = '';
+
+  while (num !== 0) {
+    const cur = num & 0xf;
+    res = cur.toString(16) + res;
+    num >>>= 4;
+  }
+
+  return res;
+}
