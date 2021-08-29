ced54ec06fb0c4029f5050ca278357be1be6e638
sabertazimi <sabertazimi@gmail.com>
2021-08-29 23:00:28 +0800

    array(leetcode-0412): fizz buzz

---
+++ leetCode/0412.test.ts
@@ -0,0 +1,36 @@
+import solution from './0412';
+
+/**
+ * @author sabertazimi
+ * @license MIT
+ * @level easy
+ * @description fizz-buzz
+ * Given an integer n, return a string array answer (1-indexed) where:
+ * answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+ * answer[i] == "Fizz" if i is divisible by 3.
+ * answer[i] == "Buzz" if i is divisible by 5.
+ * answer[i] == i if non of the above conditions are true.
+ */
+describe('LeetCode [0412]', () => {
+  test('should AC', () => {
+    expect(solution(3)).toStrictEqual(['1', '2', 'Fizz']);
+    expect(solution(5)).toStrictEqual(['1', '2', 'Fizz', '4', 'Buzz']);
+    expect(solution(15)).toStrictEqual([
+      '1',
+      '2',
+      'Fizz',
+      '4',
+      'Buzz',
+      'Fizz',
+      '7',
+      '8',
+      'Fizz',
+      'Buzz',
+      '11',
+      'Fizz',
+      '13',
+      '14',
+      'FizzBuzz',
+    ]);
+  });
+});
