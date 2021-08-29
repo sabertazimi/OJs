e129b3e787cba63c89b7ba93f367ca89e0e472f0
sabertazimi <sabertazimi@gmail.com>
2021-08-29 18:31:23 +0800

    tree(leetcode-0404): sum of left leaves

---
+++ leetCode/0404.test.ts
@@ -0,0 +1,20 @@
+import solution from './0404';
+import { arrayToTree } from './Tree';
+
+/**
+ * @author sabertazimi
+ * @license MIT
+ * @level easy
+ * @description sum-of-left-leaves
+ * Given the root of a binary tree,
+ * return the sum of all left leaves.
+ */
+describe('LeetCode [0404]', () => {
+  test('should AC', () => {
+    expect(solution(arrayToTree([0]))).toStrictEqual(0);
+    expect(solution(arrayToTree([1]))).toStrictEqual(0);
+    expect(solution(arrayToTree([3, 9, 20, null, null, 15, 7]))).toStrictEqual(
+      24
+    );
+  });
+});
