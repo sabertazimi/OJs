d0aaa454816a8d3e6c7183ae2aa2b2b13cb731c5
sabertazimi <sabertazimi@gmail.com>
2021-08-31 14:51:57 +0800

    style(leetcode-0003): longest-substring-without-repeating-characters

--- leetCode/0003.ts
+++ leetCode/0003.ts
@@ -1,20 +1,17 @@
 export default function lengthOfLongestSubstring(s: string): number {
-  if (s.length === 0) {
-    return 0;
-  }
+  if (s.length === 0) return 0;
 
-  const map = new Map<string, number>();
   let maxLen = 0;
 
   // 最大子串为 2 个重复字符间字符串长度 + 1
-  for (let lo = 0, hi = 0; hi < s.length; hi++) {
+  for (
+    let lo = 0, hi = 0, map = new Map<string, number>();
+    hi < s.length;
+    hi++
+  ) {
     const ch = s.charAt(hi);
     const index = map.get(ch);
-
-    if (index !== undefined && lo <= index) {
-      lo = index + 1;
-    }
-
+    if (index !== undefined && lo <= index) lo = index + 1;
     map.set(ch, hi);
     maxLen = Math.max(maxLen, hi - lo + 1);
   }
