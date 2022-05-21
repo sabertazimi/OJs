098ec3c27357bb849f61e64ed1bbe36ba957a551
sabertazimi <sabertazimi@gmail.com>
2022-05-22 04:38:43 +0800

    chore(deps): update dependency vitest to ^0.12.9

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    setupFiles: './tests/setupTests.ts',
+    setupFiles: 'tests/setupTests.ts',
     coverage: {
       exclude: ['tests', '**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
       reporter: ['text', 'text-summary', 'lcov'],
